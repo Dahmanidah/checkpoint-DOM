a33416c2314828a68edcac013505a539d2a25ba8
Dahmanidah <130908323+Dahmanidah@users.noreply.github.com>
2023-06-15 08:52:07 +0100

    Add files via upload

---
+++ sh.js
@@ -0,0 +1,59 @@
+const likes = document.getElementsByClassName("like");
+
+for (let i = 0; i < likes.length; i++) {
+  let like = likes[i];
+
+  like.addEventListener("click", function () {
+    if (like.style.color === "red") {
+      like.style.color = "black";
+    } else {
+      like.style.color = "red";
+    }
+  });
+}
+
+const plusses = document.getElementsByClassName("plus-btn");
+console.log(plusses);
+const UnitPrices = [1379, 176, 249];
+const prices = document.querySelectorAll(".price");
+
+for (let i = 0; i < plusses.length; i++) {
+  plusses[i].addEventListener("click", function () {
+    plusses[i].previousElementSibling.value++;
+
+    prices[i].innerText =
+      plusses[i].previousElementSibling.value * UnitPrices[i];
+    totalPrice();
+  });
+}
+const minuss = document.getElementsByClassName("minus-btn");
+
+for (let i = 0; i < minuss.length; i++) {
+  minuss[i].addEventListener("click", function () {
+    if (minuss[i].nextElementSibling.value > 1) {
+      minuss[i].nextElementSibling.value--;
+      prices[i].innerText = minuss[i].nextElementSibling.value * UnitPrices[i];
+      totalPrice();
+    }
+  });
+}
+
+const deleteButtons = document.getElementsByClassName("delete");
+const inp = document.getElementById("finalPrice");
+
+for (let i = 0; i < deleteButtons.length; i++) {
+  deleteButtons[i].addEventListener("click", function () {
+    totalPrice();
+    inp.value = parseFloat(inp.value) - parseFloat(prices[i]);
+    deleteButtons[i].parentElement.style.display = "none";
+  });
+}
+
+function totalPrice() {
+  let sum = 0;
+  for (let i = 0; i < prices.length; i++) {
+    sum += parseFloat(prices[i].innerText);
+  }
+  inp.value = sum;
+  return sum;
+}
